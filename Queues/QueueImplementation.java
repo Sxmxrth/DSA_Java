92d3d57c7a56c2bc9be5f6832cacefb45ae37819
Samarth Gupta <samarthgupta1202@gmail.com>
2022-09-12 18:13:25 +0530

    queue updated

--- Queues/QueueImplementation.java
+++ Queues/QueueImplementation.java
@@ -3,3 +3,45 @@ public class QueueImplementation {
 
     }
 }
+
+class Queue{
+    int size = 5;
+    int[] q = new int[size];
+    int front = -1;
+    int rear = -1;
+
+    public boolean isFull(){
+        if(front == 0 && rear == size-1){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty(){
+        if(front == -1 ){
+            return true;
+        }
+        return false;
+    }
+
+    public void enqueue(int x){
+        if(isFull()){
+            System.out.println("the queue is already full");
+        }
+        if (isEmpty()){
+            q[++front] = x;
+            rear = 0;
+        }
+
+        else {
+            q[++rear] = x;
+        }
+    }
+
+    public int dequeue(){
+        if(isFull()){
+            return q[front];
+        }
+
+    }
+}
