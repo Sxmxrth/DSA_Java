f8eb717e99e349c407d2cedae5770b487bbdc318
Samarth Gupta <samarthgupta1202@gmail.com>
2022-09-12 18:38:14 +0530

    queue updated

--- Queues/QueueImplementation.java
+++ Queues/QueueImplementation.java
@@ -1,5 +1,17 @@
 public class QueueImplementation {
     public static void main(String[] args) {
+        Queue queue = new Queue();
+
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.enqueue(1);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        System.out.println();
+        queue.display();
 
     }
 }
@@ -39,9 +51,26 @@ class Queue{
     }
 
     public int dequeue(){
-        if(isFull()){
-            return q[front];
+        if(isEmpty()){
+            return 0;
+        }
+        else {
+            int element = q[front];
+            if(front >= rear){
+                front = -1;
+                rear = -1;
+            }
+            else {
+                front++;
+            }
+            return element;
         }
 
+    }
+
+    public void display(){
+        for (int i = front; i <= rear ; i++) {
+            System.out.println(q[i]);
+        }
     }
 }
