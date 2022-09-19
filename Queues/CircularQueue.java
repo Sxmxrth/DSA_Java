28f27d189c2e07581cf289ea6420a0430fa1d79d
Samarth Gupta <samarthgupta1202@gmail.com>
2022-09-19 21:51:17 +0530

    Circular queue updated

--- Queues/CircularQueue.java
+++ Queues/CircularQueue.java
@@ -5,5 +5,70 @@ public class CircularQueue {
 }
 
 class CQueue{
+    int size = 5;
+    int front, rear;
+    int[] cqueue = new int[size];
 
+    CQueue(){
+        front = -1;
+        rear = -1;
+    }
+
+    public boolean isFull(){
+        if(front == 0 && rear == size-1){
+            return true;
+        }
+        if(front == rear+1){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty(){
+        if(front == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public void enqueue(int x){
+
+        if(isEmpty()){
+            front++;
+            cqueue[++rear] = x;
+        }
+
+        else if (isFull()) {
+            System.out.println("the queue is full");
+        }
+
+        else{
+            rear = (rear+1) % size;
+            cqueue[rear] = x;
+        }
+
+    }
+
+    public int dequeue(){
+        if (isEmpty()){
+            System.out.println("queue is empty");
+            return -1;
+        }
+        else{
+            int temp = cqueue[front];
+            if(front == rear){
+                front = -1;
+                rear = -1;
+            }
+            else {
+                front = (front+1) % size;
+            }
+            return temp;
+
+        }
+    }
+
+    public void display(){
+
+    }
 }
