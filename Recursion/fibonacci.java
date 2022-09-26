cca03a166ef9beeacc18795224a2d0487cbb141b
Samarth Gupta <samarthgupta1202@gmail.com>
2022-09-26 13:54:32 +0530

    Recursion file added

---
+++ Recursion/fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        // find nth fibonacci number
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n: ");
+        int n = sc.nextInt();
+        System.out.println(fibRec(n));
+    }
+
+    public static int fibRec(int n){
+        if(n < 2){
+            return n;
+        }
+        return fibRec(n-1) + fibRec(n-2);
+    }
+}
