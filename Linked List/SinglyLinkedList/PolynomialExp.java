e337de96f0b1940a7b760075d634772b12cea35f
Samarth Gupta <samarthgupta1202@gmail.com>
2022-09-27 10:25:34 +0530

    Polynomial updated

--- Linked List/SinglyLinkedList/PolynomialExp.java
+++ Linked List/SinglyLinkedList/PolynomialExp.java
@@ -2,6 +2,7 @@ package SinglyLinkedList;
 
 public class PolynomialExp {
     public static void main(String[] args) {
+
         int[] c1 = {5, 4, 2, 1};
         int[] e1 = {4, 3, 1, 0};
 
@@ -26,7 +27,10 @@ public class PolynomialExp {
 
         PolynomialExp ans = new PolynomialExp();
 
+        p1.add(p1, p2, ans);
 
+
+        //System.out.println(p1.head.next.exp);
 
     }
 
@@ -51,9 +55,9 @@ public class PolynomialExp {
         Node1 temp1 = p1.head;
         Node1 temp2 = p2.head;
 
-        while(temp1 != null){
+        while(temp1 != null && temp2 != null){
 
-            if(temp1.exp == temp2.exp){
+            if (temp1.exp == temp2.exp){
 
                 ans.push((temp1.coeff + temp2.coeff), temp1.exp);
                 temp1 = temp1.next;
@@ -61,10 +65,47 @@ public class PolynomialExp {
 
             }
 
+            else if (temp1.exp > temp2.exp){
 
+                ans.push(temp1.coeff, temp1.exp);
+                temp1 = temp1.next;
 
+            }
 
+            else {
 
+                ans.push(temp2.coeff, temp2.exp);
+                temp2 = temp2.next;
+
+            }
+
+        }
+
+        while(temp1 != null){
+
+            ans.push(temp1.coeff, temp1.exp);
+            temp1 = temp1.next;
+
+        }
+
+        while(temp2 != null){
+
+            ans.push(temp2.coeff, temp2.exp);
+            temp2 = temp2.next;
+
+        }
+
+        PrintAns(ans);
+
+    }
+
+    public void PrintAns(PolynomialExp ans){
+
+        Node1 temp = ans.head;
+
+        while (temp != null){
+            System.out.print(temp.coeff + "x^" + temp.exp+" ");
+            temp = temp.next;
         }
 
     }
