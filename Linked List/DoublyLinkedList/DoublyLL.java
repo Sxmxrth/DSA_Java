50d4cc8683a1dda0be85d563a7c6b64c09f476bf
Samarth Gupta <samarthgupta1202@gmail.com>
2022-09-28 19:08:59 +0530

    updated DLL

--- Linked List/DoublyLinkedList/DoublyLL.java
+++ Linked List/DoublyLinkedList/DoublyLL.java
@@ -3,12 +3,23 @@ package DoublyLinkedList;
 public class DoublyLL {
     public static void main(String[] args) {
 
+        DoublyLL d = new DoublyLL();
+
+        d.push(12);
+        d.push(1);
+        d.push(14);
+        d.push(-9);
+        d.push(10);
+
+        d.display();
+
     }
 
     Node head = null, tail = null;
     public void push( int key ){
 
         Node new_node = new Node(key);
+
         if(head == null){
 
             head = tail =  new_node;
@@ -17,6 +28,24 @@ public class DoublyLL {
 
         }
 
+        else {
+
+            new_node.prev = tail;
+            tail.next = new_node;
+            tail = new_node;
+            tail.next = null;
+
+        }
+    }
+
+    public void display(){
+
+        Node temp = head;
+
+        while (temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
 
     }
 }
