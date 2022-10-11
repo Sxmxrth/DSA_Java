8f79b619592da53b7dc98b9b1a8e93a53bbaf72f
Samarth Gupta <samarthgupta1202@gmail.com>
2022-10-11 09:38:32 +0530

    Binary tree updated

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -4,8 +4,8 @@ public class BinaryTree {
         BinaryTree b = new BinaryTree();
         Node root = new Node(5);
 
-        b.insert(root, 1);
         b.insert(root, 4);
+        b.insert(root, 23);
         b.insert(root, 2);
         b.insert(root, 8);
         b.insert(root, 6);
@@ -14,6 +14,14 @@ public class BinaryTree {
         b.insert(root, 9);
 
         b.InOrder(root);
+
+        System.out.println(b.search(root, 12));
+
+        b.findMin(root);
+
+        b.findMax(root);
+
+
 
     }
 
@@ -35,6 +43,43 @@ public class BinaryTree {
             else{
                 node.right = new Node(value);
             }
+        }
+
+    }
+
+    public Node search(Node root, int target){
+
+        if(root == null || root.key == target){
+            return root;
+        }
+
+        if (root.key < target) {
+            return search(root.right, target);
+        }
+
+        return search(root.left, target);
+
+    }
+
+    public void findMin(Node root){
+
+        if(root.left == null){
+            System.out.println("Min is " + root.key);
+        }
+
+        else {
+            findMin(root.left);
+        }
+    }
+
+    public void findMax(Node root){
+
+        if (root.right == null){
+            System.out.println("Max is " + root.key);
+        }
+
+        else {
+            findMax(root.right);
         }
 
     }
